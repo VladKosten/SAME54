80af7a881379b152fc59c540955826e8c374a6a5
Vlad Kosten <vladkosten@gmail.com>
2025-03-17 22:18:28 +0300

    s

--- main.c
+++ main.c
@@ -1,18 +1,39 @@
 #include <atmel_start.h>
 #include <app_startup.h>
 
+static void setIrqPrioNVIC(const IRQn_Type irqType, const uint8_t irqPrio);
+
 int main(void)
 {
-	/* Initializes MCU, drivers and middleware */
-	atmel_start_init();
+    /* Initializes MCU, drivers and middleware */
+    atmel_start_init();
 
+    for (int i = 0; i < 4; i++)
+    {
+        setIrqPrioNVIC(SERCOM2_0_IRQn + i, 4u);
+    }
 
-	/* Application startup */
-	AppStartup();
+    /* Application startup */
+    AppStartup();
 
-	/* Replace with your application code */
-	while (1) {
+    /* Replace with your application code */
+    while (1)
+    {
 
-		ASSERT(0);
-	}
+        ASSERT(0);
+    }
+}
+
+/**
+ * \brief      Set irq priority
+ * \param[in]  no;
+ * \param[out] no;
+ * \return     no.
+ */
+static void setIrqPrioNVIC(const IRQn_Type irqType, const uint8_t irqPrio)
+{
+    NVIC_DisableIRQ(irqType);
+    NVIC_ClearPendingIRQ(irqType);
+    NVIC_SetPriority(irqType, irqPrio);
+    NVIC_EnableIRQ(irqType);
 }
